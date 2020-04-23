053ca506f5a708d997420f16b4dfd3afbb86d986
dhirajdgandhi <ddgandhi.96@gmail.com>
2020-04-23 14:01:48 -0400

    pyPlot

--- plotsales.py
+++ plotsales.py
@@ -1,18 +1,15 @@
 from matplotlib import pyplot as plt
 
-xBefore = [0.1, 0.2, 0.3, 0.4]
-yBefore = [1, 4, 9, 16]
+userMovieList = [1, 2, 3, 4]
+cfRating = [1, 4, 9, 16]
+contentRating = [2, 3, 12, 15]
+sentimentRating = [14, 3, 2, 12]
+trueRating = [12, 5, 7, 8]
 
-xAfter = [0.1, 0.2, 0.3, 0.4]
-yAfter = [1, 2, 3, 4]
 
-file_object = open("")
-lines = file_object.readlines()
-
-for line in lines:
-    print(line)
-
-plt.plot(xAfter, yAfter, label='During COVID-19')
-plt.plot(xBefore, yBefore, label='Before COVID-19')
+plt.plot(userMovieList, cfRating, label='COLLAB')
+plt.plot(userMovieList, contentRating, label='CONTENT BASED')
+plt.plot(userMovieList, sentimentRating, label='SENTIMENT')
+plt.plot(userMovieList, trueRating, label='ACTUAL')
 plt.legend()
 plt.show()
